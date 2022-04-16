4144d99aab915362ffdbb83e8e191a95e010e265
sysy-s <szymon.bold@gmail.com>
2022-04-16 11:51:09 +0200

    fixed filenames not working on windows

--- routes/recipes.js
+++ routes/recipes.js
@@ -21,7 +21,7 @@ const storage = multer.diskStorage({
     cb(null, "./images/");
   },
   filename: function (req, file, cb) {
-    cb(null, new Date().toISOString() + file.originalname);
+    cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
   },
 });
 
